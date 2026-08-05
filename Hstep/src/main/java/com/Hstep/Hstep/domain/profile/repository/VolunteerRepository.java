1b3bd4fb585bdfeb0a8c95eb054e0145a71b1c3b
hee0543 <chaehee0543@gmail.com>
2026-08-05 12:06:37 +0900

    feat: Volunteer레포지토리에 소유권 체크용 세ì 메드 추가

--- Hstep/src/main/java/com/Hstep/Hstep/domain/profile/repository/VolunteerRepository.java
+++ Hstep/src/main/java/com/Hstep/Hstep/domain/profile/repository/VolunteerRepository.java
@@ -3,7 +3,9 @@ package com.Hstep.Hstep.domain.profile.repository;
 import com.Hstep.Hstep.domain.profile.entity.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
     List<Volunteer> findByMember_UserId(String userId);
+    Optional<Volunteer> findByVolunteerIdAndMember_UserId(Long volunteerId, String userId);
 }
