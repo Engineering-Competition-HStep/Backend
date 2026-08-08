9622fc1d2ee2504f4b253a23241adc5423fb71ba
hee0543 <chaehee0543@gmail.com>
2026-08-09 01:54:04 +0900

    feat: chatroom 엔터티에 scenario 필드 추가

--- Hstep/src/main/java/com/Hstep/Hstep/domain/chat/entity/ChatRoom.java
+++ Hstep/src/main/java/com/Hstep/Hstep/domain/chat/entity/ChatRoom.java
@@ -1,5 +1,6 @@
 package com.Hstep.Hstep.domain.chat.entity;
 
+import com.Hstep.Hstep.domain.chat.constant.ChatScenario;
 import com.Hstep.Hstep.domain.member.entity.Member;
 import com.Hstep.Hstep.global.entity.BaseEntity;
 import jakarta.persistence.*;
@@ -21,12 +22,17 @@ public class ChatRoom extends BaseEntity {
     @Column(name = "title", length = 100)
     private String title;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "scenario", nullable = false, length = 30)
+    private ChatScenario scenario;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private Member member;
 
-    public ChatRoom(String title, Member member) {
+    public ChatRoom(String title, ChatScenario scenario, Member member) {
         this.title = title;
+        this.scenario = scenario;
         this.member = member;
     }
 
