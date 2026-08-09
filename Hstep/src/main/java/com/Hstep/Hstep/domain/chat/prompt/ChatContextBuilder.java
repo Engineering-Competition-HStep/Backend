2d2803d05c0bf8880f982b234ab7622167387d9f
hee0543 <chaehee0543@gmail.com>
2026-08-09 12:55:44 +0900

    feat: LLM이 읽게 된 학생 사실 정보 파일 ChatContextBuilder 생성

---
+++ Hstep/src/main/java/com/Hstep/Hstep/domain/chat/prompt/ChatContextBuilder.java
@@ -0,0 +1,34 @@
+package com.Hstep.Hstep.domain.chat.prompt;
+
+public class ChatContextBuilder {
+
+    public static String build(
+            String track1, String track2,
+            Integer grade, String overallGpa,
+            String certificates, String awards,
+            String volunteers, String activities,
+            String roadmapTemplate
+    ) {
+        return """
+                [학생 정보]
+                - 1트랙: %s
+                - 2트랙: %s
+                - 학년: %d학년
+                - 평균학점: %s
+
+                [보유 스펙]
+                - 자격증: %s
+                - 수상경력: %s
+                - 자원봉사: %s
+                - 기타활동: %s
+
+                [트랙 기준 추천 로드맵]
+                %s
+                """.formatted(
+                track1, track2 != null ? track2 : "없음",
+                grade, overallGpa != null ? overallGpa : "미입력",
+                certificates, awards, volunteers, activities,
+                roadmapTemplate
+        );
+    }
+}
